35d9ce4da8f122624db5c260731357701813428b
vigneshmb <vigneshmb2016@gmail.com>
2021-04-22 16:06:25 +0530

    Divided the Previous Homepage into Modular Components
    Home Page is divided into
    
    1. Home Page
    2. Directory
    3. Menu Item

---
+++ src/components/menu-item/menu-item.component.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+
+const MenuItem=(props)=>(
+
+    <div className={`${props.size} menu-item`}>
+    <div className='background-image' style={{
+        backgroundImage:`url(${props.imageUrl})`
+    }}
+    />
+        <div className='content'>
+            <h1 className='title'>{props.title.toUpperCase()}</h1>
+            <span className='subtitle'>Shop Now</span>    
+        </div>
+    </div>    
+);
+
+
+export default MenuItem;
