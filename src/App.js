5f496df6b8076f7763940072b46ad873dc171473
vigneshmb <vigneshmb2016@gmail.com>
2021-04-23 13:59:08 +0530

    Added Shop Page
    With Preview of 4 items in every category

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
 import {Switch,Route} from 'react-router-dom';
 
 
-const Hats=()=>(
-  <div>
-  <h1>HATS PAGE</h1>
-  </div>
-)
+import HomePage from './pages/homepage/homepage.component';
+import Shop from './pages/shop/shop.component';
+
+
 
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route exact path='/hats' component={Hats}/>
+      <Route exact path='/shop' component={Shop}/>
       </Switch>
     </div>
   );
---
+++ src/components/collection-preview/collection.preview.component.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './collection.preview.component.styles.scss';
+
+import CollectionItem from '../collection-item/collection.item.component'
+
+const CollectionPreview=({title,items})=>(
+    <div className='collection-preview'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+            <div className='preview'>
+            {
+                items
+                .filter((item,idx)=>idx<4)
+                .map(({id,...itemprops})=>(
+                    <CollectionItem key={id} {...itemprops}/>
+                ))
+            }    
+            </div>
+    </div>
+
+);
+
+
+export default CollectionPreview;
