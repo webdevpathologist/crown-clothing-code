b57b7306f8de983bc563e51410c58436a5967c52
vigneshmb <vigneshmb2016@gmail.com>
2021-04-23 14:19:20 +0530

    Added Header component
    With React Router Link option

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {Switch,Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route exact path='/shop' component={Shop}/>
