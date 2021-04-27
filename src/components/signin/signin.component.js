e2ea4e5d38994289c801add4225dfff66f46c423
vigneshmb <vigneshmb2016@gmail.com>
2021-04-27 11:58:02 +0530

    Sign In With Email Completed
    And Title of  the App is also changed

--- src/components/signin/signin.component.js
+++ src/components/signin/signin.component.js
@@ -4,7 +4,7 @@ import './signin.styles.scss';
 
 import FormInput from '../form-input/form.input.component';
 import CustomButton from '../custom-button/custom.button.component';
-import {signinwithGoogle} from '../firebase/firebase.utils';
+import {auth,signinwithGoogle} from '../firebase/firebase.utils';
 
 
 class SignIn extends React.Component{
@@ -16,10 +16,23 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit=event=>{
+    handleSubmit= async event=>{
         event.preventDefault();
 
-        this.setState({email:'',password:''})
+        const {email,password}=this.state;
+
+        try {
+
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email:'',password:''});
+            
+        } catch (error) {
+            alert('please enter valid credentials');
+            console.log(error.message);
+            
+        }
+
+        
     
     }
 
